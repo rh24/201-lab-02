6be20089d4a82e2afe8ee6a14b80c18857369654
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-07-31 14:21:52 -0700

    Add function to validate input

--- js/app.js
+++ js/app.js
@@ -3,32 +3,43 @@
 // Design a guessing game about yourself in yes/no format
 
 function aboutMe() {
+    // You can also set variables to true/false, and set the prompts to userAnswer
     let bornInLa;
     let loveBagels;
     let brokenBone;
     let bingeWatch;
     let amazingBowler;
 
+    function validInput(input) {
+        return input.toLowerCase() === "yes" || input.toLowerCase() === "no";
+    }
+
     function question1() {
-        bornInLa = prompt('Was I born in Los Angeles County?').toLowerCase();
-        console.log(bornInLa);
-        // if (bornInLA === )
+        bornInLa = prompt(`Was I born in Los Angeles County?`).toLowerCase();
+
+        if (!validInput(bornInLa)) {
+            alert(`This is a yes or no question.`);
+        } else if (bornInLa === "yes") {
+            alert(`You're wrong! I was born in NYC in the borough of Queens.`)
+        } else if (bornInLa === "no") {
+            alert(`Correct! I was actually born in Flushing, NY.`)
+        }
     }
     
     function question2() {
-        loveBagels = prompt('Do I love bagels?').toLowerCase();
+        loveBagels = prompt(`Do I love bagels?`).toLowerCase();
     }
     
     function question3() {
-        brokenBone = prompt('Have I ever broken a bone?').toLowerCase();
+        brokenBone = prompt(`Have I ever broken a bone?`).toLowerCase();
     }
     
     function question4() {
-        bingeWatch = prompt('Do I binge watch TV?').toLowerCase();
+        bingeWatch = prompt(`Do I binge watch TV?`).toLowerCase();
     }
     
     function question5() {
-        amazingBowler = prompt('Am I an amazing bowler?').toLowerCase();
+        amazingBowler = prompt(`Am I an amazing bowler?`).toLowerCase();
     }
 
     question1();
