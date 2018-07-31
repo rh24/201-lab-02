37bcab5dec5966a97c3720331865c1b8934a7e17
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-07-31 12:04:41 -0700

    Add first 3 questions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-alert('testing javascript')
+// alert('testing javascript')
+
+// Design a guessing game about yourself in yes/no format
+function question1() {
+    prompt('Was I born in Los Angeles County?');
+}
+
+function question2() {
+    prompt('Do I love bagels?');
+}
+
+function question3() {
+    prompt('Is my last name \"Hong\"?');
+}
+
+function question4() {
+
+}
+
+funciton question5() {
+
+}
+
+question1();
+question2();
+question3();
