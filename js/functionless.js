1008dde5f1425002447be8862862c88861c1559e
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-01 16:46:04 -0700

    Fix all bugs in question flow

--- js/functionless.js
+++ js/functionless.js
@@ -1,56 +1,68 @@
 'use strict';
 
-// // Create one huge multidimensional array that stores the first five questions and answers.
-// const questionsAndAnswers = [
-//   [
-//     'Was I born in Los Angeles County',
-//     'Do I love bagels',
-//     'Have I ever broken a bone',
-//     'Do I binge watch TV',
-//     'Am I an amazing bowler'
-//   ],
-//   [
-//     'no',
-//     'yes',
-//     'no',
-//     'yes',
-//     'no'
-//   ]
-// ];
+// Create one huge multidimensional array that stores the first five questions and answers.
+const questionsAndAnswers = [
+  [
+    'Was I born in Los Angeles County',
+    'Do I love bagels',
+    'Have I ever broken a bone',
+    'Do I binge watch TV',
+    'Am I an amazing bowler'
+  ],
+  [
+    'no',
+    'yes',
+    'no',
+    'yes',
+    'no'
+  ]
+];
 
-// // Acceptable answers
-// const validInputs = ['yes', 'no', 'y', 'n'];
+// Acceptable answers
+const validInputs = ['yes', 'no', 'y', 'n'];
 
-// // Variable to store sanitized version of user's input
-// let sanitizedInput;
+// Variable to store sanitized version of user's input
+let sanitizedInput;
 
-// // Greet the user
-// const userName = prompt('What\'s your name ?');
-// alert(`Hey, ${userName}. Thanks for visiting my About Me page.`);
+// Store correct vs incorrect answers
+let tally = {
+  correct: 0,
+  incorrect: 0
+};
 
-// // Ask user questions about me
-// for (let j = 0; j < questionsAndAnswers[0].length; j++) {
-//   let userInput = prompt(questionsAndAnswers[0][j] + '?').toLowerCase();
+// Greet the user
+const userName = prompt('What\'s your name ?');
+alert(`Hey, ${userName}. Thanks for visiting my About Me page.`);
 
-//   if (userInput === 'y' || userInput === 'yes') {
-//     sanitizedInput = 'yes';
-//     console.log('User answer: ' + sanitizedInput);
-//   } else if (userInput === 'n' || userInput === 'no') {
-//     sanitizedInput = 'no';
-//     console.log('User answer: ' + sanitizedInput);
-//   } else {
-//     while (!validInputs.includes(userInput)) {
-//       alert('Please, enter Y/N or YES/NO.');
-//       userInput = prompt(questionsAndAnswers[0][j] + '?').toLowerCase();
-//     }
-//   }
+// Ask user questions about me
+for (let j = 0; j < questionsAndAnswers[0].length; j++) {
+  let userInput = prompt(questionsAndAnswers[0][j] + '?').toLowerCase();
 
-//   // Alert users if they got the right or wrong answer
-//   sanitizedInput === questionsAndAnswers[1][j] ? alert('Correct!') : alert('Wrong!');
-// }
+  if (userInput === 'y' || userInput === 'yes') {
+    sanitizedInput = 'yes';
+    console.log('User answer: ' + sanitizedInput);
+  } else if (userInput === 'n' || userInput === 'no') {
+    sanitizedInput = 'no';
+    console.log('User answer: ' + sanitizedInput);
+  } else {
+    while (!validInputs.includes(userInput)) {
+      alert('Please, enter Y/N or YES/NO.');
+      userInput = prompt(questionsAndAnswers[0][j] + '?').toLowerCase();
+    }
+  }
 
-// // Sixth question:
-// // Ask the user a numeric guessing game that accepts exactly 4 guesses.
+  // Alert users if they got the right or wrong answer
+  if (sanitizedInput === questionsAndAnswers[1][j]) {
+    tally.correct++;
+    alert('Correct!');
+  } else {
+    tally.incorrect++;
+    alert('Wrong!');
+  }
+}
+
+// Sixth question:
+// Ask the user a numeric guessing game that accepts exactly 4 guesses.
 
 const countriesVisited = [
   'New Zealand',
@@ -78,31 +90,47 @@ const countriesVisited = [
   'Dominican Republic'
 ];
 
-// // This is the first attempt
-// let guessingGame = Number(prompt('Can you guess how many countries I\'ve visited?'));
+// This is the first attempt
+let guessingGame = Number(prompt('Can you guess how many countries I\'ve visited?'));
 
-// // Continuation of user attempts
-// let attempt = 2;
+// Continuation of user attempts
+let attempt = 2;
+let message;
 
-// while (attempt < 5) {
-//   if (guessingGame === countriesVisited.length) {
-//     alert('Correct!');
-//     break;
-//     // If the user enters a non-number value, they'll be prompted again.
-//   } else if (!guessingGame) {
-//     guessingGame = Number(prompt('Please enter a number. Can you guess how many countries I\'ve visited?'));
-//   } else {
-//     guessingGame = Number(prompt('Try again! Can you guess how many countries I\'ve visited?'));
+while (attempt < 5) {
+  if (guessingGame === countriesVisited.length) {
+    alert('Correct!');
+    tally.correct++;
+    break;
+    // If the user enters a non-number value, they'll be prompted again.
+  } else if (!guessingGame) {
+    guessingGame = Number(prompt('Please enter a number. Can you guess how many countries I\'ve visited?'));
+  } else if (guessingGame < countriesVisited.length) {
+    guessingGame = Number(prompt('Guess higher...'));
+    // Only increment attemps if they're valid inputs
+    attempt++;
+  } else if (guessingGame > countriesVisited.length) {
+    message = Number(prompt('Guess lower...'));
+    attempt++;
+  }
 
-//     // Only increment attemps if they're valid inputs
-//     attempt++;
-//   }
-// }
+  // I can't get this switch statement to work.
+  // switch (guessingGame) {
+  // case guessingGame < countriesVisited.length:
+  //   message = Number(prompt('Guess higher...'));
+  //   break;
+  // case guessingGame > countriesVisited.length:
+  //   message = Number(prompt('Guess lower...'));
+  //   break;
+  // default:
+  //   message = 'test';
+  // }
+}
 
-// // Answer is revealed after 4 failed attempts
-// if (attempt === 5) {
-//   alert(`I've been to ${countriesVisited.length} countries.`);
-// }
+// Answer is revealed after 4 failed attempts
+if (attempt === 5) {
+  alert(`I've been to ${countriesVisited.length} countries.`);
+}
 
 
 
@@ -111,15 +139,18 @@ const countriesVisited = [
 // Joining, then adding a ', ' after the guess seems like a hack-y way to solve this. Is it better to iterate over the entire array in order to find a match?
 let guessWhich = prompt('Can you guess a country I\'ve been to? Enter one country name per try.').toLowerCase() + ', ';
 let tryNumber = 1;
+const matchString = countriesVisited.join(', ').toLowerCase();
 
 while (tryNumber < 7) {
-  const matchString = countriesVisited.join(', ').toLowerCase();
 
   console.log(`Attempt ${tryNumber}: ${guessWhich}`);
 
   if (!matchString.includes(guessWhich)) {
     guessWhich = prompt('I haven\'t been there. Try again...').toLowerCase() + ', ';
-  } else {
+  }
+
+  if (matchString.includes(guessWhich)) {
+    tally.correct++;
     alert(`That's right! ${guessWhich[0].toUpperCase() + guessWhich.slice(1)} was a lot of fun. You should check it out sometime.`);
     break;
   }
@@ -128,3 +159,7 @@ while (tryNumber < 7) {
 }
 
 // Upon using up the tries OR getting one right, all the possible answers display.
+
+
+// Keep a tally of all the questions the user has gotten correct
+alert(`You got ${tally.correct}/${tally.incorrect + tally.correct} on this quiz. Thanks for playing!!`);
