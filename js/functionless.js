af90cbba34c2fe398f590a2597636b809f9a12d4
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-01 13:26:29 -0700

    Create multidimensional array of questions and answers

---
+++ js/functionless.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Create one huge multidimensional array that stores the first five questions and answers.
+const questionsAndAnswers = [
+  [
+    'no',
+    'yes',
+    'no',
+    'yes',
+    'no'
+  ],
+  [
+    'Was I born in Los Angeles County',
+    'Do I love bagels',
+    'Have I ever broken a bone',
+    'Do I binge watch TV',
+    'Am I an amazing bowler'
+  ]
+];
+
