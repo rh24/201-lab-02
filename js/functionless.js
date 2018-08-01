f40ee3c5d5416ad2a7bc7a656f2c1e1a7e3ef845
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-01 14:30:05 -0700

    Sanitize user input and re-prompt if invalid

--- js/functionless.js
+++ js/functionless.js
@@ -3,24 +3,50 @@
 // Create one huge multidimensional array that stores the first five questions and answers.
 const questionsAndAnswers = [
   [
-    'no',
-    'yes',
-    'no',
-    'yes',
-    'no'
-  ],
-  [
     'Was I born in Los Angeles County',
     'Do I love bagels',
     'Have I ever broken a bone',
     'Do I binge watch TV',
     'Am I an amazing bowler'
+  ],
+  [
+    'no',
+    'yes',
+    'no',
+    'yes',
+    'no'
   ]
 ];
+
+const validInputs = ['yes', 'no', 'y', 'n'];
+let sanitizedInput;
 
 // Greet the user
 const userName = prompt('What\'s your name ?');
 alert(`Hey, ${userName}. Thanks for visiting my About Me page.`);
 
+// Ask user questions about me
+for (let j = 0; j < questionsAndAnswers[0].length; j++) {
+  let userInput = prompt(questionsAndAnswers[0][j] + '?').toLowerCase();
 
-
+  if (userInput === 'y' || userInput === 'yes') {
+    sanitizedInput = 'yes';
+    console.log('User answer: ' + sanitizedInput);
+  } else if (userInput === 'n' || userInput === 'no') {
+    sanitizedInput = 'no';
+    console.log('User answer: ' + sanitizedInput);
+  } else {
+    while (!validInputs.includes(userInput)) {
+      alert('Please, enter Y/N or YES/NO.');
+      userInput = prompt(questionsAndAnswers[0][j] + '?').toLowerCase();
+    }
+  }
+  
+  // for (let i = 0; i < questionsAndAnswers[1].length; i++) {
+  //   if (sanitizedInput === questionsAndAnswers[1][i]) {
+  //     alert('Correct!');
+  //   } else {
+  //     alert('Wrong!');
+  //   }
+  // }
+}
